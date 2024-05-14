5432a784da3a84e43c1b101e4d743dc7ed1b0078
paa04 <paa04@list.ru>
2024-05-14 21:17:54 +0300

    style

--- Lab_04/modules/basic_func.py
+++ Lab_04/modules/basic_func.py
@@ -1,5 +1,4 @@
-def append_points_circ(points : list, x : int, y : int, x_center : int, y_center : int, alpha : int):
-    
+def append_points_circ(points: list, x: int, y: int, x_center: int, y_center: int, alpha: int):
     points.extend([
         [x, y, alpha],
         [2 * x_center - x, y, alpha],
@@ -11,12 +10,11 @@ def append_points_circ(points : list, x : int, y : int, x_center : int, y_center
         [-y + x_center + y_center, -x + y_center + x_center, alpha]
     ])
 
-def append_points_ellipse(points : list, x : int, y : int, x_center : int, y_center : int, alpha : int):
-    
+
+def append_points_ellipse(points: list, x: int, y: int, x_center: int, y_center: int, alpha: int):
     points.extend([
         [x, y, alpha],
         [2 * x_center - x, y, alpha],
         [x, 2 * y_center - y, alpha],
         [2 * x_center - x, 2 * y_center - y, alpha],
     ])
-
